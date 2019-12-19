f7f15389ad834f1111df5fb3b542bd8323b94c66
lalize <printf@kakao.com>
2019-12-19 17:43:13 +0900

    refactor: 자동차 위치를 int -> Position 클래스로 변경

--- src/main/java/domain/car/Car.java
+++ src/main/java/domain/car/Car.java
@@ -1,8 +1,10 @@
 package domain.car;
 
 public class Car {
+	private static final int INITIAL_POSITION = 0;
+
 	private final Name name;
-	private int position = 0;
+	private final Position position = new Position(INITIAL_POSITION);
 
 	public Car(String name) {
 		this.name = new Name(name);
