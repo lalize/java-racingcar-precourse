9a7aa16a716c68ef2051444b74834ea5654b0304
lalize <printf@kakao.com>
2019-12-19 17:26:56 +0900

    feat: 시도 횟수를 나타내는 Round 클래스 추가

---
+++ src/main/java/domain/Round.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.Iterator;
+
+public class Round implements Iterator<Integer> {
+	private static final int MIN = 1;
+	private static final String MIN_ERROR = "횟수가 " + MIN + "미만 이다.";
+
+	private int round;
+
+	public Round(int round) {
+		validate(round);
+		this.round = round;
+	}
+
+	public static void validate(int round) {
+		if (round < MIN) {
+			throw new IllegalArgumentException(MIN_ERROR);
+		}
+	}
+
+	@Override
+	public boolean hasNext() {
+		return round >= MIN;
+	}
+
+	@Override
+	public Integer next() {
+		return round--;
+	}
+}
