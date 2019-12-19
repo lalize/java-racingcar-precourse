d4b752b73b7ac27142cc4c9e442cf76eeec48937
lalize <printf@kakao.com>
2019-12-19 17:20:08 +0900

    feat: 시도할 횟수를 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,9 +6,15 @@ public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 	private static final String GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String CAR_NAMES_DELIMITER = ",";
+	private static final String GET_ROUND_MESSAGE = "시도할 횟수는 몇회인가요?";
 
 	public static String[] getCarNames() {
 		System.out.println(GET_CAR_NAMES_MESSAGE);
 		return SCANNER.nextLine().split(CAR_NAMES_DELIMITER);
+	}
+
+	public static int getRound() {
+		System.out.println(GET_ROUND_MESSAGE);
+		return Integer.parseInt(SCANNER.nextLine());
 	}
 }
