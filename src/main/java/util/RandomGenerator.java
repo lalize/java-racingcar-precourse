ec4563ef9a7458dbca7368fdbd9d88a0cbb1cf9c
lalize <printf@kakao.com>
2019-12-19 19:44:25 +0900

    refactor: 클래스 상수 대문자로 수정

--- src/main/java/util/RandomGenerator.java
+++ src/main/java/util/RandomGenerator.java
@@ -3,10 +3,10 @@ package util;
 import java.util.Random;
 
 public class RandomGenerator {
-	private static final Random random = new Random();
+	private static final Random RANDOM = new Random();
 	private static final int BOUND = 10;
 
 	public static int next() {
-		return random.nextInt(BOUND);
+		return RANDOM.nextInt(BOUND);
 	}
 }
