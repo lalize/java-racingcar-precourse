898e7fe99857e6989fb66fc9c9d90a147c427167
lalize <printf@kakao.com>
2019-12-19 17:09:11 +0900

    feat: 자동차 이름 유효성 검사 기능 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,41 @@
 package domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private static final int MAX_NAME_SIZE = 5;
+	private static final String BLANK = " ";
+	private static final String NAME_EMPTY_ERROR = "이름이 비어있다.";
+	private static final String NAME_BLANK_ERROR = "이름에 공백이 존재한다.";
+	private static final String NAME_SIZE_ERROR = "이름이 " + MAX_NAME_SIZE + "자를 초과한다.";
 
-    public Car(String name) {
-        this.name = name;
-    }
+	private final String name;
+	private int position = 0;
 
-    // 추가 기능 구현
+	public Car(String name) {
+	    validate(name);
+		this.name = name;
+	}
+
+	public static void validate(String name) {
+		checkEmpty(name);
+		checkBlank(name);
+		checkSize(name);
+	}
+
+	private static void checkEmpty(String name) {
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException(NAME_EMPTY_ERROR);
+		}
+	}
+
+	private static void checkBlank(String name) {
+		if (name.contains(BLANK)) {
+			throw new IllegalArgumentException(NAME_BLANK_ERROR);
+		}
+	}
+
+	private static void checkSize(String name) {
+		if (name.length() > MAX_NAME_SIZE) {
+			throw new IllegalArgumentException(NAME_SIZE_ERROR);
+		}
+	}
 }
