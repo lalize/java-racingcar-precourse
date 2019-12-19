81a8280a069a98a1456b39ceb8e40ed5155287c8
lalize <printf@kakao.com>
2019-12-19 17:49:50 +0900

    feat: 자동차가 전진하는 기능 추가

--- src/main/java/domain/car/Car.java
+++ src/main/java/domain/car/Car.java
@@ -1,12 +1,25 @@
 package domain.car;
 
+import util.RandomGenerator;
+
 public class Car {
 	private static final int INITIAL_POSITION = 0;
+	private static final int SIGNAL = 4;
 
 	private final Name name;
 	private final Position position = new Position(INITIAL_POSITION);
 
 	public Car(String name) {
 		this.name = new Name(name);
+	}
+
+	private boolean hasSignal() {
+		return RandomGenerator.next() >= SIGNAL;
+	}
+
+	public void moveIfHasSignal() {
+		if (hasSignal()) {
+			position.plusOne();
+		}
 	}
 }
